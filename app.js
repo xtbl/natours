3c77b00cd90000947530704a1dfa7ef96a677c1f
Cristobal <correodecristobal@gmail.com>
2020-09-10 06:57:39 -0600

    add eslint

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
-const { restart } = require('nodemon');
-
+const {
+    restart
+} = require('nodemon');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
-
 const app = express();
 
 // MIDDLEWARE
 console.log(process.env.NODE_ENV)
-if(process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 app.use(express.json());
